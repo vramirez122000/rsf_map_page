8b76b8880a3dfc22b19044fd711ed80ff3e24203
kevincfrech <49885257+kevincfrech@users.noreply.github.com>
2020-04-12 18:53:45 -0400

    added tooltips

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -11,9 +11,24 @@ export class Legend extends Component {
         </div>
                 
         <div id='legend2'>
-            <div>
-            <h4>Change in <br></br>
-            Median Tax Bill</h4>
+            <div class="title_container">
+                <h4 class = 'title'>Change in <br></br>
+                Median Tax Bill</h4>
+                <div>
+                    <div class="tooltip"> <img src="data/info-tooltip.png" id="median_tax_tooltip"></img>
+                        <span class="tooltiptext">The map at city-level view shows the difference in the
+                                                  median household tax bill as a percentage difference 
+                                                  from the current scenario.
+                                                    <br></br>
+                                                    <br></br>
+                                                  Use the slider at the bottom of the page to show the projected  difference in median tax bill based on how much of the citys property revenue comes from a tax on land value.
+                                                    <br></br>
+                                                    <br></br>
+                                                  Click on each zipcode area for more information.
+                                                    </span>
+                    </div>
+                 
+                </div>
             </div>
             
             <div id = 'color_grad' class = 'legend_col' >
@@ -42,29 +57,47 @@ export class Legend extends Component {
                     <div class = 'row'><p>-100 %</p></div>
            </div>
         
-          <div id = 'parcel_title'>
-            <h4>Parcel Types</h4>
+          <div id = 'parcel_title' class ='title_container'>
+            <h4 class='title'>Parcel Types</h4>
+                <div>
+                    <div class="tooltip"> <img src="data/info-tooltip.png" id="parcel_tooltip"></img>
+                        <span class="tooltiptext">Zoom in to street level to see individual parcels.
+                                                    <br></br>
+                                                    <br></br>
+                                                  Click on a parcel to see how each projected tax scenario would affect a propertys 
+                                                  tax bill. 
+                                                    <br></br>
+                                                    <br></br>
+                                                  Please be patient while parcel data loads. 
+                                                    </span>
+                    </div>
+                 
+                </div>
             </div>
             <div id = 'parcel_legend'> 
                 <div class='row'>
-                <span id='residential_dot'></span>
-                <p>Residential</p>
-                    </div>
+                    <div id='residential_dot'></div>
+                    <p>Residential</p>
+                </div>
+            
                 <div class='row'>    
-                <span id='commercial_dot'></span>
-                <p>Commercial</p>
+                    <div id='commercial_dot'></div>
+                    <p>Commercial</p>
                     </div>
+            
                 <div class='row'>
-                <span id='industrial_dot'></span>
-                <p>'Industrial'</p>
+                    <div id='industrial_dot'></div>
+                    <p>Industrial</p>
+                </div>
+            
+                <div class='row'>
+                    <div id='vacant_dot'></div>
+                    <p>Vacant Land</p>
                     </div>
+            
                 <div class='row'>
-                <span id='vacant_dot'></span>
-                <p>Vacant Land</p>
-                    </div>
-                <div class='row'>
-                <span id='exempt_dot'></span>
-                <p>Exempt</p>
+                    <div id='exempt_dot'></div>
+                    <p>Exempt</p>
                     </div>
             
                 </div>
